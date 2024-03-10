5fa5c73fd6b48dcecb2d899d84e4f182f20dcc72
D2356 <158171203+D2356@users.noreply.github.com>
2024-03-10 15:04:29 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,4 @@
-const moonLanding = new Date("March 10, 24 14:27:18");
+const moonLanding = new Date("4 10, 24 14:27:18");
 
 console.log(moonLanding.getFullYear());
 console.log(moonLanding.getMonth());
